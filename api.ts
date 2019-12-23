2fd7929e6b779b4068070340480edc5ee4e93842
calosliu <whulc@outlook.com>
2019-12-23 23:58:12 +0800

    [Cong] Add basic files

---
+++ api.ts
@@ -0,0 +1,7 @@
+export class Api {
+	constructor() {}
+
+	test(action: string): void {
+		console.log(`this is test function ${action}`);
+	};
+}
---
+++ index.ts
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+import { Api } from './api';
+
+import commander = require('commander');
+
+const program = new commander.Command();
+const api = new Api();
+
+program
+	.version('0.0.1')
+	.option('-t --test', 'test', api.test('test'));
+
